36fdc6bc28707504064fb3ad10e9c7323e8eed3b
Adrienne <adriennepaquin@gmail.com>
2021-05-26 15:26:00 -0400

    commit tree 07015858fece02b1dceec2a8a0133ec224d98c82

--- main.js
+++ main.js
@@ -3,10 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.getElementById('modal')
+modal.className = "hidden"
 
+let bodyTag = document.querySelector('body')
+bodyTag.addEventListener("click", handleClick)
 
-
-
+function handleClick(event){
+  //console.log(event.target.className === "like-glyph")
+  if (event.target.innerHTML === EMPTY_HEART){
+    let thisId = event.target.parentNode.parentNode.parentNode.parentNode.id
+    //console.log(thisId)
+    mimicServerCall()
+    //change heart to full heart, add .activated-heart class (make heart red))
+    .then(makeRed(thisId))
+    .catch()
+    }
+  }
+  function makeRed(id){
+    let heart = document.getElementById(`${id}`)
+    let actualHeart = heart.querySelector('footer span')
+    console.log(actualHeart)
+    actualHeart.textContent = FULL_HEART
+    actualHeart.className = "activated-heart"
+  }
+  function makeEmpty(id){
+    let heart = document.getElementById(`${id}`)
+    let actualHeart = heart.querySelector('footer span')
+    console.log(actualHeart)
+    actualHeart.textContent = EMPTY_HEART
+    actualHeart.className.remove()
+  }
+  function error(){
+    document.getElementById('modal').hidden = false
+  }
+    
+  // change the text content of the id glyph to FULL_HEART
+  
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
